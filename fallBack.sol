b4a24e042d638830a46e517644abb9982f06413e
Mufrad6502 <mufradmustahsin152@gmail.com>
2025-08-02 17:04:30 +0600

    fallback function modify

--- fallBack.sol
+++ fallBack.sol
@@ -2,14 +2,18 @@
 pragma solidity ^0.8.0;
 
 contract Fallback {
+        event Log(string func, address sender, uint value, bytes data);
+
     // Fallback function to handle plain Ether transfers
     fallback() external payable {
         // Logic to execute when the contract receives Ether
+        emit Log("fallback", msg.sender, msg.value, msg.data);
     }
 
     // Receive function to handle direct Ether transfers
     receive() external payable {
         // Logic to execute when the contract receives Ether directly
+        emit Log("receive", msg.sender, msg.value, "");
     }
 
     // Function to check the contract's balance
