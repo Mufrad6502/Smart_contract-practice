04f1c46758203d539ce492bc5fe341f2107705ac
Mufrad6502 <mufradmustahsin152@gmail.com>
2025-08-02 17:41:33 +0600

    sendETH function

---
+++ sendETH.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SendETH {
+    constructor() payable {}
+
+    receive () external payable {
+        // Logic to handle incoming Ether
+    }
+function sendETHTransfer(address payable _to, uint256 _amount) public {
+        require(address(this).balance >= _amount, "Insufficient balance");
+        _to.transfer(_amount);
+    }
+}
