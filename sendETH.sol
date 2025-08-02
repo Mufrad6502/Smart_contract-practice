25f24c7aa08fdb370db34412b2949a18e75c8d0d
Mufrad6502 <mufradmustahsin152@gmail.com>
2025-08-02 18:21:14 +0600

    call function

--- sendETH.sol
+++ sendETH.sol
@@ -11,6 +11,13 @@ function sendETHTransfer(address payable _to, uint256 _amount) public {
         require(address(this).balance >= _amount, "Insufficient balance");
         _to.transfer(_amount);
     }
+
+
+function sendETHCall(address payable _to, uint256 _amount) public {
+        require(address(this).balance >= _amount, "Insufficient balance");
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Transfer failed");
+    }
 }
 
 contract EthReceiver{
